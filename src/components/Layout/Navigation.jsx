c882fea11306e05ef3ce26cb37b362a3f71618bc
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-01-03 00:16:11 +0330

    add mobile version for home page

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -1,17 +1,39 @@
 import styles from "./navigation.module.css";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/shared/logo.svg";
+import hamburgetIcon from "../../assets/shared/icon-hamburger.svg";
+import closeIcon from "../../assets/shared/icon-close.svg";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuOpenStyle = {
+    right: isMenuOpen ? "0" : "-100%",
+    transition: "500ms ease all"
+  };
+
+  const menuStatusHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logoPart}>
         <div className={styles.logoPartInbox}>
           <img src={logo} alt="" />
         </div>
+        <button
+          onClick={menuStatusHandler}
+          className={`${styles.hamburgetBtn}`}
+        >
+          <img src={`${isMenuOpen ? closeIcon : hamburgetIcon}`} alt="" />
+        </button>
       </div>
-      <div className={styles.navigatePart}>
-        <ul className={`${styles.navgitationList} space-x-10`}>
+      <div style={menuOpenStyle} className={`${styles.navigatePart}`}>
+        <ul
+          className={`${styles.navgitationList} space-y-5 md:space-y-0 md:space-x-10`}
+        >
           <NavLink
             to="/"
             className={({ isActive }) =>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,15 +5,15 @@ const Home = () => {
   return (
     <section className={`${styles.homeContainer}`}>
       <div className={styles.heroData}>
-        <h3 className="uppercase font-barlow text-lightblue lg:text-3xl xl:text-4xl tracking-wider">
+        <h3 className="text-center lg:text-left uppercase font-barlow text-lightblue text-lg lg:text-3xl xl:text-4xl tracking-wider">
           SO, YOU WANT TO TRAVEL TO
         </h3>
         <h2
-          className={`${styles.homeHeader} uppercase text-white font-bellefair`}
+          className={`${styles.homeHeader} text-center lg:text-left uppercase text-white font-bellefair`}
         >
           SPACE
         </h2>
-        <p className="font-barlow text-lightblue text-lg tracking-wider leading-loose text-justify">
+        <p className="text-center lg:text-justify font-barlow text-lightblue lg:text-lg tracking-wider leading-loose">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
@@ -23,7 +23,7 @@ const Home = () => {
       <div className={styles.heroExplore}>
         <Link to="destination">
           <button
-            className={`${styles.exploreBtn} font-bellefair text-dark text-3xl`}
+            className={`${styles.exploreBtn} font-bellefair text-dark text-2xl lg:text-3xl`}
           >
             EXPLORE
           </button>
