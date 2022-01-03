622f5e29e908c50ee1302712a604f1b1ad05d1a5
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-01-03 12:53:05 +0330

    change background for each route

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
-import styles from "./layout.module.css";
+import { useLocation } from "react-router-dom";
+import "./layout.css";
 import Navigation from "./Navigation";
 
 const Layout = (props) => {
+  const path = useLocation().pathname;
+  const location = path.split("/")[1];
+
   return (
-    <div className={styles.backgroundWrapper}>
+    <div className={`backgroundWrapper ${location}`}>
       <Navigation />
       <main>{props.children}</main>
     </div>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <section className={`${styles.homeContainer}`}>
       <div className={styles.heroData}>
-        <h3 className="text-center lg:text-left uppercase font-barlow text-lightblue text-lg lg:text-3xl xl:text-4xl tracking-wider">
+        <h3 className="text-center lg:text-left uppercase font-barlow text-lightblue text-lg md:text-3xl xl:text-4xl tracking-wider">
           SO, YOU WANT TO TRAVEL TO
         </h3>
         <h2
@@ -13,12 +13,14 @@ const Home = () => {
         >
           SPACE
         </h2>
-        <p className="text-center lg:text-justify font-barlow text-lightblue lg:text-lg tracking-wider leading-loose">
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+        <div className="flex justify-center">
+          <p className="w-full max-w-lg lg:max-w-none text-center lg:text-justify font-barlow text-lightblue lg:text-lg tracking-wider leading-loose">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
       </div>
       <div className={styles.heroExplore}>
         <Link to="destination">
