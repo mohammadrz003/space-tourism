b282d0285c4071bb34449d4e34a7835be8579298
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-01-04 13:06:21 +0330

    add dynamic Image for Planets

--- src/views/Destination/Destination.jsx
+++ src/views/Destination/Destination.jsx
@@ -1,42 +1,55 @@
 import styles from "./destination.module.css";
+import { useState } from "react";
 import moon from "../../assets/destination/image-moon.webp";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import data from "../../data.json";
 import "react-tabs/style/react-tabs.css";
 
 const Destination = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const planetImgPath = data.destinations[tabIndex].images.webp;
+
   return (
     <section className={styles.destinationContainer}>
-      <h3 className="font-barlow text-white text-3xl tracking-widest uppercase">
+      <h3 className="text-center lg:text-left font-barlow text-white text-xl lg:text-3xl tracking-widest uppercase">
         <span className="opacity-25 mr-4">01</span>Pick your destination
       </h3>
-      <div className="flex py-10 lg:space-x-28 xl:space-x-40">
-        <div className="w-1/2 flex justify-end items-center">
-          <img className={styles.planetImg} src={moon} alt="" />
+      <div className="flex flex-col lg:flex-row py-10 lg:space-x-28 xl:space-x-40">
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end items-center">
+          <img className={styles.planetImg} src={planetImgPath} alt="" />
         </div>
-        <div className="w-1/2">
-          <Tabs selectedTabClassName={styles.tabCategory}>
+        <div className="w-full mt-8 lg:w-1/2">
+          <Tabs
+            selectedIndex={tabIndex}
+            onSelect={(tabIndex) => setTabIndex(tabIndex)}
+            selectedTabClassName={styles.tabCategory}
+          >
             <TabList
-              className={`${styles.tabHeader} font-barlow lg:text-lg xl:text-xl text-lightblue space-x-5`}
+              className={`${styles.tabHeader} flex justify-center lg:justify-start font-barlow lg:text-lg xl:text-xl text-lightblue space-x-5`}
             >
-              {data.destinations.map((element) => {
-                return <Tab className={styles.eachTab}>{element.name}</Tab>;
+              {data.destinations.map((element, index) => {
+                return (
+                  <Tab key={index} className={styles.eachTab}>
+                    {element.name}
+                  </Tab>
+                );
               })}
             </TabList>
-            {data.destinations.map((element) => {
+            {data.destinations.map((element, index) => {
               return (
-                <TabPanel>
+                <TabPanel key={index}>
                   <h2
-                    className={`${styles.destinationPlanetName} text-white font-bellefair uppercase`}
+                    className={`${styles.destinationPlanetName} mt-5 lg:mt-0 text-center lg:text-left text-white font-bellefair uppercase`}
                   >
                     {element.name}
                   </h2>
-                  <p className="font-barlow text-lightblue text-base tracking-wider">
+                  <p className="text-center lg:text-left font-barlow text-lightblue text-base tracking-wider">
                     {element.description}
                   </p>
                   <hr className="border-none bg-darkGray h-px my-8" />
-                  <ul className="flex space-x-14">
-                    <li className="flex flex-col uppercase">
+                  <ul className="flex flex-col items-center lg:flex-row space-y-5 lg:space-y-0 lg:space-x-14">
+                    <li className="flex flex-col items-center lg:items-start uppercase">
                       <span className="text-lightblue font-barlow text-lg">
                         AVG. DISTANCE
                       </span>
@@ -44,7 +57,7 @@ const Destination = () => {
                         {element.distance}
                       </span>
                     </li>
-                    <li className="flex flex-col uppercase">
+                    <li className="flex flex-col items-center lg:items-start uppercase">
                       <span className="text-lightblue font-barlow text-lg">
                         Est. travel time
                       </span>
